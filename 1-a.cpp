fc761dd9b8b5921ca3b410bd4ecefb3078f51be6
ParmitaRajthala <159704614+ParmitaRajthala@users.noreply.github.com>
2025-06-07 20:14:43 +0545

    Add files via upload

---
+++ 1-a.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define SIZE 100
+
+int stack[SIZE];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == SIZE - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed to stack\n", value);
+    }
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+// Peek operation
+void peek() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Top element is %d\n", stack[top]);
+    }
+}
+
+// Display all stack elements
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements: ");
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n--- Stack Menu ---\n");
+        printf("1.Push  2.Pop  3.Peek  4.Display  5.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Enter the value to push: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            printf("Exiting...\n");
+            printf("\n Name=Parmita Rajthala \n Roll No = 30 \n Lab No =1-a");
+            return 0;
+        default:
+            printf("Invalid choice. Try again.\n");
+        }
+    } 
+    
+}
+
